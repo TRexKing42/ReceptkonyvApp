51f1140355fb32739be3621724ab61a1cd764121
TRexKing42 <bkristof110@gmail.com>
2020-01-11 18:28:11 +0100

    Firebase Login

--- app/src/main/java/com/banfikristof/receptkonyv/MainActivity.java
+++ app/src/main/java/com/banfikristof/receptkonyv/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,21 +19,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         NavigationView.OnNavigationItemSelectedListener,
         ReceptekFragment.ReceptekFragmentListener {
 
+    private static final int RC_SIGN_IN = 123;
     private DrawerLayout dl;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private NavigationView nv;
+    public FirebaseUser user;
+    private TextView headerName, headerEmail;
+    private ImageView headerPicture;
 
     private SQLiteDBHelper DBManager;
 
@@ -47,7 +61,12 @@ public class MainActivity extends AppCompatActivity implements
     private void init() {
         dl = findViewById(R.id.drawerlayout);
         nv = findViewById(R.id.mainmenu);
+        headerName = findViewById(R.id.menu_header_nev);
         nv.setNavigationItemSelectedListener(this);
+        View header = nv.getHeaderView(0);
+        headerName = header.findViewById(R.id.menu_header_nev);
+        headerEmail = header.findViewById(R.id.menu_header_email);
+        headerPicture = header.findViewById(R.id.menu_header_pic);
 
         DBManager = new SQLiteDBHelper(this);
 
@@ -60,6 +79,44 @@ public class MainActivity extends AppCompatActivity implements
         fragmentTransaction.commit();
     }
 
+    private void firebaseSignInIntent() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                headerName.setText(user.getDisplayName());
+                headerPicture.setImageURI(user.getPhotoUrl());
+                headerEmail.setText(user.getEmail());
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -72,6 +129,9 @@ public class MainActivity extends AppCompatActivity implements
                 break;
             case R.id.menu_receptek:
                 selectedFragment = new ReceptekFragment();
+                break;
+            case R.id.menu_login:
+                firebaseSignInIntent();
                 break;
             default:
                 Toast.makeText(MainActivity.this, "Még nem",Toast.LENGTH_SHORT).show();
